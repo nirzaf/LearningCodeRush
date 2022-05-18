2b0f8ff688ca32c7e781e682044dda544917e5b9
nirzaf <mfmfazrin1986@gmail.com>
2022-05-18 19:06:18 +0530

    nnn

--- CapsInsert/AddMissingConstructors.cs
+++ CapsInsert/AddMissingConstructors.cs
@@ -5,7 +5,13 @@
 //`              ![](ArrowDown;;;0.008,0.008) Press Caps+Insert to add missing constructors.
 public class AddMissingConstructors : BaseClass
 {
+	public AddMissingConstructors(int value) : base(value)
+	{
+	}
 
+	public AddMissingConstructors(int value, string name) : base(value, name)
+	{
+	}
 }
 //`![](BtnMoreLikeThis;crcommand:OpenFile:AddToInterface.cs;;0.012,0.012) << Add to Interface
 
